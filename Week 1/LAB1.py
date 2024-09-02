42fc7638f2cf228b7ea4c366ec479a06a6a9d01d
Henrykim1111 <henrykim9319@gmail.com>
2024-09-02 19:52:58 -0400

    Q 4 finished

--- Week 1/LAB1.py
+++ Week 1/LAB1.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 # %%
 # Q1
-mean, variance = map(float, input("Enter the mean, variance: ").split(", "))
-mean = mean if mean else 0
+mu, variance = map(float, input("Enter the mean, variance: ").split(", "))
+mu = mu if mu else 0
 
 num_observations = int(input("Enter the number of observations: "))
 
-x = np.random.normal(mean, 1, 1000)
+x = np.random.normal(mu, 1, 1000)
 y = np.random.normal(5, np.sqrt(2), 1000)
 
 print("First three values of x:", x[:3])
@@ -19,10 +19,10 @@ print("First three values of y:", y[:3])
 numerator, x_denominator, y_denominator = 0.0, 0.0, 0.0
 
 for i in range(0, len(x)):
-    numerator += (x[i]-mean)*(y[i]-5)
+    numerator += (x[i]-mu)*(y[i]-5)
 
 for i in range(0, len(x)):
-    x_denominator += (x[i]-mean)**2
+    x_denominator += (x[i]-mu)**2
 
 for i in range(0, len(x)):
     y_denominator += (y[i]-5)**2
@@ -41,3 +41,15 @@ print("The sample mean of random variable x is :", x.mean())
 print("The sample mean of random variable y is :", y.mean())
 print("The sample variance of random variable x is :", x.var())
 print("The sample variance of random variable y is :", y.var())
+
+# %%
+# Q4
+
+plt.hist(x)
+plt.hist(y)
+plt.xlabel("Sample Index")
+plt.ylabel("Value")
+plt.title("Histogram of x and y")
+plt.legend()
+
+plt.show()
