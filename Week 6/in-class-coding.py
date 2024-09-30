342f9a429c89812fed48ae20ce00d338199908f2
Henrykim1111 <henrykim9319@gmail.com>
2024-09-30 14:12:33 -0400

    Twin Plot

--- Week 6/in-class-coding.py
+++ Week 6/in-class-coding.py
@@ -34,5 +34,75 @@ plt.show()
 
 #%%
 import pandas as pd
-df = pd.read_excel('Sample - Superstore.xls')
-print(df.head())
+url = 'https://github.com/rjafari979/Information-Visualization-Data-Analytics-Dataset-/raw/refs/heads/main/Sample%20-%20Superstore.xls'
+df = pd.read_excel(url)
+
+plt.figure(figsize=(10,5))
+plt.title('Region-wise Revenue', fontsize = 18)
+
+Top_region = df.loc[:,['Quantity', 'Region', 'Sales', 'Discount', 'Profit']].groupby('Region').sum().sort_values('Sales', ascending=False)
+Top_region.reset_index(inplace=True)
+plt.bar(Top_region['Region'], Top_region['Sales'], color='#FF6F61', label = 'Sales')
+plt.bar(Top_region['Region'], Top_region['Quantity'], color='green', edgecolor = 'Red', linewidth = 1, label = 'Quantity')
+plt.xlabel("Region", fontsize = 15)
+plt.xticks(fontsize = 12, rotation = 45)
+plt.yticks(fontsize = 12)
+plt.legend()
+
+# Add numerical value of sales to the bar plot
+for k,v in Top_region['Sales'].astype('int').items():
+    if v > 250000:
+        plt.text(k, v - 75000, '$'+str(v), fontsize = 12, color = 'k', horizontalalignment='center')
+    else:
+        plt.text(k, v - 15000, '$'+str(v), fontsize = 12, color = 'k', horizontalalignment='center')
+
+plt.tight_layout()
+plt.show()
+
+#%% 3D Plot
+def f(x1, x2):
+    return (x1 + x2)**4 - 12*x1*x2 + x1 + x2 + 1
+
+x = np.linspace(-1, 1, 50)
+y = np.linspace(-1, 1, 50)
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_wireframe(X, Y, Z)
+plt.tight_layout()
+plt.show()
+
+#%% Contour Plot
+plt.figure(figsize=(8,8))
+level = np.arange(-3, 5, .5)
+CS = plt.contour(X, Y, Z, level, colors ='k')
+plt.clabel(CS, inline =1, fontsize = 10)
+
+plt.tight_layout()
+plt.title('Simplest default with labels')
+plt.axis('equal')
+plt.xlim([-1,1])
+plt.ylim([-1,1])
+plt.show()
+
+#%% Twin Plot
+x = np.arange(1,6)
+y1 = np.array([10, 15, 7, 12, 9])
+y2 = np.array([200, 300, 150, 250, 180])
+fig, ax1 = plt.subplots(figsize=(8,4))
+ax1.bar(x, y1, color = 'b', alpha = 0.7, label = 'Sales')
+ax1.set_xlabel('Month')
+ax1.set_ylabel('Sales', color='b')
+ax1.set_ylim(0,20)
+
+ax2=ax1.twinx()
+ax2.plot(x, y2, color = 'r', marker = 'o', label = 'Revenue')
+ax2.set_ylabel('Revenue', color = 'r')
+ax2.set_ylim(0,400)
+fig.legend(loc='upper left', bbox_to_anchor = (0.15,.85))
+plt.title('Sales and Revenue Comparison')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
